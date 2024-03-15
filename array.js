d750577b319f06dac3c00ffba6423cbdae6079c1
Ishraaaat <160309674+Ishraaaat@users.noreply.github.com>
2024-03-15 11:08:49 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,26 @@
+const array1 = ["Banana", "Orange", "Apple", "Mango"];
+const array2 = [12,4,8,9];
+ let index=array1[0]
+ console.log(index)
+
+ const array3 = [2,"Kunchala",5,["Sanjana",7]];
+  let res =array3[3]
+  console.log(res)
+
+  const array4 = [2,"Kunchala",5,["Sanjana",7]];
+  let tes =array4[3][0]
+  console.log(tes)
+
+ let arr=[1,"String",0,["Sourabh",50]]
+  console.log(arr[3][0],arr[3][1])
+
+  let arr1=["Karen",67,"Bodmas"]
+  console.log(typeof(arr1))
+  console.log(typeof(null))
+
+  let array=[1,2,3,4]
+  console.log(typeof(array))
+
+  const array5=[1,2,3]
+  array5.push(4)
+  console.log(array5)
